a929914aea99a66377d93133ea20367d8cb75199
yblhlk <2174176006@qq.com>
2024-06-03 20:33:15 +0800

    学习如何修改线程函数的实参： 1. 引用传参 + std::ref()函数 2. 指针传参 3. lambda表达式的捕捉列表

--- C++11/多线程/线程库(thread).cpp
+++ C++11/多线程/线程库(thread).cpp
@@ -19,6 +19,10 @@ void doop2()
 		cout << 22 << endl;
 }
 
+void foo(string s1) {
+	std::cout << s1 << " Thread ID: " << std::this_thread::get_id() << std::endl;
+}
+
 int main()
 {
 	// 0. 理解线程对象的作用 : 并行执行
@@ -118,8 +122,18 @@ int main()
 
 	// 3.测试get_id()
 	{
+		// 线程类thread的get_id()方法返回当前线程的唯一标识符。
+		// 普通函数可以使用td::this_thread::get_id()来获取当前线程的唯一标识符。
 		cout << "================3.测试get_id()===============" << endl;
+		std::thread t1(foo, "T1");
+		std::thread t2(foo, "T2");
 
+		std::cout << "Main Thread ID: " << std::this_thread::get_id() << std::endl;
+		std::cout << "Thread 1 ID: " << t1.get_id() << std::endl;
+		std::cout << "Thread 2 ID: " << t2.get_id() << std::endl;
+
+		t1.join();
+		t2.join();
 	}
 
 	// 4.测试detach()
@@ -134,13 +148,56 @@ int main()
 
 		// 其实detach()主要是用来分离线程的，让线程的生命周期不再受thread对象的控制。
 		// 当你对一个std::thread对象调用detach()方法后，该线程就不再与原始的std::thread对象关联，也就是说，原始的std::thread对象不再管理这个线程的生命周期。
-		thread t3(doop1);
-		t3.detach();
+		/*thread t3(doop1);
+		t3.detach();*/
+		//t3.join(); //报错
 		// 现象：不使用detach()分离线程，主程序结束后线程对象还没有结束会报错。（“abort() has been called”崩溃：在这里是因为主线程尝试结束而子线程还在运行，并且由于线程对象没有被join()等待，所以其析构函数会尝试进行所谓的“终止”）
 		// 使用detach()分离线程和thread对象后，不报错。
 		// 原因：因为此时线程不由thread对象管理，不会造成主程序关闭，thread对象未关闭的情况。
 		// 分离的线程也由操作系统来接管。（分离的线程不会随着程序的“正常”关闭而“自动”终止。它们是被操作系统作为进程的一部分而终止的。）
 
 	}
+
+	// 5. 线程函数如何修改实参？
+	{
+		// 线程函数的参数是以值拷贝的方式拷贝到线程栈空间中的，
+		// 因此：即使线程参数为引用类型，在线程中修改后也不能修改外部实参，因为其实际引用的是线程栈中的拷贝，而不是外部实参
+		
+		// 如果要通过线程函数的形参改变外部的实参（输出型参数），可以参考以下三种方式：
+		// 引用传参 + std::ref()函数、 指针、 lambda表达式的捕捉列表
+
+		// 观察0： 线程函数的参数为引用类型时，直接传参会报错
+		void modify_1(int& x);
+		int a = 0;
+		//std::thread t0(modify_1, a); //运行时报错
+		// 结论：如果线程函数的参数为引用类型，直接传参会报错
+		
+		// 观察1： 使用 引用传参 + std::ref()函数 来修改实参
+		void modify_1(int& x);
+		a = 0;
+		std::thread t1(modify_1, ref(a));
+		cout << a << endl;
+		t1.join();
+		cout << a << endl; //线程函数结束后(join()后)才能在主线程中观察到修改，因为线程是并行执行的。
+		// 结论1：线程函数的引用类型参数，要用std::ref()函数封装后传递才能正常使用。
+		// 结论2：要在线程函数结束后才能在其他线程中看到结果，最好用join()来保证线程结束。
+
+		// 观察2： 使用 指针 来修改实参
+		void modify_2(int* x);
+		std::thread t2(modify_2, &a); //& :取地址符
+		cout << a << endl;
+		t2.join();
+		cout << a << endl; //线程函数结束后(join()后)才能在主线程中观察到修改
+		// 结论：指针参数可以正常修改实参
+
+		// 观察3：lambda表达式的捕捉列表
+		std::thread t3([&a]{a = 3; });
+		cout << a << endl;
+		t3.join();
+		cout << a << endl; //线程函数结束后(join()后)才能在主线程中观察到修改
+	}
 	return 0;
 }
+
+void modify_1(int& x) { x = 1; }
+void modify_2(int* x) { *x = 2; }
